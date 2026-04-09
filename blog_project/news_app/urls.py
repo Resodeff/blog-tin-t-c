10d393c7065d2316f7f4fa2699caa080cad82d29
Resodeff <supbogalaxy12345@gmail.com>
2026-04-09 09:08:39 +0700

    Cập nhật file .env và cấu hình Database Render

--- blog_project/news_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home_view, name = 'home'),
-    path('POST/<int:id>/', views.post_detail, name = 'post_detail')
-]
-
