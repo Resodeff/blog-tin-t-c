ec64e03e9b129b1713a936d244d1a8f9522b9ebf
Nguyen Van Truyen <0306241419@caothang.edu.vn>
2026-04-10 21:38:16 +0700

    Them chuc nang dang nhap dang ky

--- news_app/urls.py
+++ news_app/urls.py
@@ -1,8 +1,14 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name = 'home'),
-    path('POST/<int:id>/', views.post_detail, name = 'post_detail')
-]
+    path('', views.home_view, name='home'),
+    path('POST/<int:id>/', views.post_detail, name='post_detail'),
+    #code cuar Truyền ở đây
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
 
+    #==============================================================
+]
--- news_app/views.py
+++ news_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import POST, CATEGORY
 from django.db.models import Q
 
@@ -33,3 +33,51 @@ def post_detail(request, id):
     }
 
     return render(request, 'post_detail.html', context)
+
+#code của Truyền ở đây
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    return render(request, 'register.html', {'form': form})
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    error = None
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            error = 'Sai tên đăng nhập hoặc mật khẩu'
+
+    return render(request, 'login.html', {'error': error})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+#==================================================================
